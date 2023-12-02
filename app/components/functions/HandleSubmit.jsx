eb820a927d6cc391d9450fb00ff49f017fb00459
Daniel McGrath <danmcgrath1035@gmail.com>
2023-12-01 18:00:14 -0600

    fixin

--- app/components/functions/HandleSubmit.jsx
+++ app/components/functions/HandleSubmit.jsx
@@ -8,7 +8,7 @@ export async function HandleSubmit(email, subject, message) {
     const resend = new Resend(process.env.RESEND_API_KEY);
 
     const {data} = await resend.emails.send({
-        from: 'Danny <danny@dannymportfolio.com>',
+        from: 'WebsiteForm <danny@dannymportfolio.com>',
         to: ['danmcgrath1035@gmail.com'],
         subject: subject,
         react: MessageUsEmail({name: email, email, message})
